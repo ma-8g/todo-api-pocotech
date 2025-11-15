a5f276a6653f24f5b0cf5f1c6daf978935d8872b
KT <aprgxy@gmail.com>
2025-11-15 16:05:14 +0900

    - build.gradle: アプリ起動に必要なライブラリを追加 - ApiController.java: GETメソッドをオーバーライドしてHTTP200OKを返す。http://localhost:8080/api/todos

--- src/main/java/com/example/todo_api_pocotech/controller/ApiController.java
+++ src/main/java/com/example/todo_api_pocotech/controller/ApiController.java
@@ -1,6 +1,17 @@
 package com.example.todo_api_pocotech.controller;
 
 import com.example.todoapi.controller.ApiApi;
+import com.example.todoapi.model.Todo;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+@RestController
 public class ApiController implements ApiApi {
+
+    @Override
+    public ResponseEntity<List<Todo>> getTodos() {
+        return ResponseEntity.ok().build();
+    }
 }
