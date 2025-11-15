5da6718d8e7eca0076e4ee66aa2e5ce2616152ac
KT <aprgxy@gmail.com>
2025-11-15 13:26:16 +0900

    最初のコミット

---
+++ src/main/java/com/example/todo_api_pocotech/TodoApiPocotechApplication.java
@@ -0,0 +1,13 @@
+package com.example.todo_api_pocotech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoApiPocotechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoApiPocotechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/todo_api_pocotech/TodoApiPocotechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.todo_api_pocotech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApiPocotechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
