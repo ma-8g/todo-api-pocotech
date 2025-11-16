bf03bf4f696558c8d3877d97194753cb7b1df47b
KT <aprgxy@gmail.com>
2025-11-16 17:01:01 +0900

    - application.properties: h2DBへの接続情報追加。 - build.gradle: h2DBへの接続情報追加。 - data.sql:データ追加 - schema.sql:テーブル追加 localhost:8080/h2-console で開く。url=jdbc:h2:mem:todoapi;

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO tasks(title) VALUES ('タスク1');
+INSERT INTO tasks(title) VALUES ('タスク2');
+INSERT INTO tasks(title) VALUES ('タスク3');
+INSERT INTO tasks(title) VALUES ('タスク4');
+INSERT INTO tasks(title) VALUES ('タスク5');
