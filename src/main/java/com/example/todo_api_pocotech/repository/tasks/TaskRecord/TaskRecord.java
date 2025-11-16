7766b10168cb6b6de0fe886deafec09893bd1cba
KT <aprgxy@gmail.com>
2025-11-16 20:28:26 +0900

    57_Repository実装 - TaskRepository.java: MyBatisでDBデータ取得 - TaskRecord.java:DBから取得したデータを格納する。Optional<TaskRecord>を返す。@Valueアノテーションを付与。 - TaskService.java: TaskRepository#select() を実行。レコードがない場合は例外実行。

---
+++ src/main/java/com/example/todo_api_pocotech/repository/tasks/TaskRecord/TaskRecord.java
@@ -0,0 +1,9 @@
+package com.example.todo_api_pocotech.repository.tasks.TaskRecord;
+
+import lombok.Value;
+
+@Value
+public class TaskRecord {
+    long id;
+    String title;
+}
--- src/main/java/com/example/todo_api_pocotech/service/tasks/TaskService.java
+++ src/main/java/com/example/todo_api_pocotech/service/tasks/TaskService.java
@@ -1,10 +1,21 @@
 package com.example.todo_api_pocotech.service.tasks;
 
+import com.example.todo_api_pocotech.repository.tasks.TaskRepository.TaskRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class TaskService {
+    private final TaskRepository taskRepository;
+
     public TaskEntity find(){
-        return new TaskEntity(2, "from Service");
+        return taskRepository.select().map(record ->
+            new TaskEntity(
+                record.getId(),
+                record.getTitle()
+            )
+        ).orElseThrow(() -> new IllegalStateException("TODO")); //TODO
+
     }
 }
