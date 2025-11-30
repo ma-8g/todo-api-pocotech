f3ec6e35f2868a1eb88aabd49fafaffe078ca22e
KT <aprgxy@gmail.com>
2025-11-30 09:58:33 +0900

    78,79_400badrequest：スキーマ - CustomExceptionHandler.java: /tasks エンドポイントへの不正なリクエストボディに BadRequestError を返す。

--- src/main/java/com/example/todo_api_pocotech/controller/advice/CustomExceptionHandler.java
+++ src/main/java/com/example/todo_api_pocotech/controller/advice/CustomExceptionHandler.java
@@ -1,18 +1,38 @@
 package com.example.todo_api_pocotech.controller.advice;
 
 import com.example.todo_api_pocotech.service.tasks.TaskEntityNotFoundException;
+import com.example.todoapi.model.BadRequestError;
 import com.example.todoapi.model.ResourceNotFoundError;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @RestControllerAdvice
-public class CustomExceptionHandler {
+public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(TaskEntityNotFoundException.class)
     public ResponseEntity<ResourceNotFoundError> handleTaskEntityNotFoundException(TaskEntityNotFoundException e) {
         var error = new ResourceNotFoundError();
         error.setDetail(e.getMessage());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
     }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        // /tasks エンドポイントへの不正なリクエストボディ（title が null の場合など）に対して BadRequestError を返すはずだが500エラーになるため実行されない。
+        return ResponseEntity.badRequest().body(new BadRequestError());
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        // /tasks エンドポイントへの不正なリクエストボディ（「"title":」）の場合に BadRequestError を返す
+        return ResponseEntity.badRequest().body(new BadRequestError());
+    }
+
 }
